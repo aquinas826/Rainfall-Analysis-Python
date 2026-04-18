a3038f35a81344d7716a5c7020a6904487e01b88
aquinas826 <aquinas684@gmail.com>
2026-04-18 23:50:08 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,8 +1,10 @@
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pandas as pd
 
 df = pd.read_csv("Sub_Division_IMD_2017.csv")
+
+print(df.describe())
 
 # Histogram
 plt.hist(df[df.columns[1]], bins=30, color="skyblue")
@@ -15,6 +17,7 @@ sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.title("Correlation Heatmap")
 plt.show()
 
+
 # Violin plot
 sns.violinplot(x=df.columns[1], y=df.columns[2], data=df)
 plt.title("Rainfall Distribution Visualization")
@@ -23,3 +26,4 @@ plt.show()
 # Pairplot
 sns.pairplot(df[df.select_dtypes(include=['float64','int64']).columns[:5]])
 plt.show()
+
