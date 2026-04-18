c20da30216a1b3a02e50917f38f5a3afba04fb23
aquinas826 <aquinas684@gmail.com>
2026-04-18 23:30:55 +0530

    Create eda.py

---
+++ eda.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+df = pd.read_csv("Sub_Division_IMD_2017.csv")
+
+# Histogram
+plt.hist(df[df.columns[1]], bins=30, color="skyblue")
+plt.title("Histogram of Rainfall")
+plt.show()
+
+# Correlation heatmap
+plt.figure(figsize=(10,8))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+plt.title("Correlation Heatmap")
+plt.show()
